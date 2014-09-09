02955fa4e1fc9b2d56114aa7b054c5ba58b81a1e
Yet Nguyen <tienyettoan@gmail.com>
2014-09-09 17:56:02 -0500

    R code

---
+++ hw1.R
@@ -0,0 +1,32 @@
+#2.1
+install.packages("binhf")
+library(binhf)
+mu <- rep(c(2.64, 2.28, 1.82, 1.40), each = 3)
+px_y <- mu
+p0_y <- rep(mean(mu), 12)
+
+
+
+sst <- (norm(px_y, 0)^2 - norm(p0_y, 0)^2)
+sst
+mse <- 0.3675
+df1 <- 3
+df2 <- 8
+ftest <- (sst/df1)/(mse)
+1 - pf(ftest, df1, df2)
+
+# 2.3 
+
+inv.s.lambda <- 60/(sqrt(3/2) +2* sqrt(11/36) + 2* sqrt(11/36) + 3*sqrt(11/54))
+ 
+n1 <- sqrt(3/2)*inv.s.lambda
+n2 <- n3 <- sqrt(11/36)*inv.s.lambda
+n4 <- sqrt(11/54)*inv.s.lambda
+
+round(c(n1, n2, n3, n4)) + 1 
+t(round(c(n1, n2, n3, n4)) +1)%*%c(1, 2, 2, 3)
+
+
+# 2.4
+
+# The 
